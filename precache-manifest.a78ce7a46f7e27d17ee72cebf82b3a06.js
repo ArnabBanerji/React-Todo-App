d14ccf76e5d49f1b14f47438b3dd4755d278bb04
Huzaif <huzaifwasim.nitrr@gmail.com>
2019-05-18 18:01:36 +0530

    Updates

---
+++ precache-manifest.a78ce7a46f7e27d17ee72cebf82b3a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939ca830c5e02013617fb2ff8f0b08f7",
+    "url": "/Huzaif-Wasim/React-Todo-App.git/index.html"
+  },
+  {
+    "revision": "226a1b967343a7c5713e",
+    "url": "/Huzaif-Wasim/React-Todo-App.git/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/Huzaif-Wasim/React-Todo-App.git/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "226a1b967343a7c5713e",
+    "url": "/Huzaif-Wasim/React-Todo-App.git/static/js/main.257526f7.chunk.js"
+  },
+  {
+    "revision": "7a68684c0fe9f1aa4b32",
+    "url": "/Huzaif-Wasim/React-Todo-App.git/static/js/runtime~main.1047ce97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Huzaif-Wasim/React-Todo-App.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
